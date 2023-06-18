1b26d3e5e3b6b8de9d5cae733c8061f42a99412b
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-18 19:42:24 +0530

    observables

--- src/app/homeO/homeO.component.ts
+++ src/app/homeO/homeO.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, interval } from'rxjs';
+import { timeout } from 'rxjs-compat/operator/timeout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeOComponent implements OnInit {
 
+  // storing all our observables.
+  private firstObsSubscriptions: Subscription;
+  
   constructor() { }
 
   ngOnInit() {
+    // observable
+    // this.firstObsSubscriptions = interval(period: 10000).subscribe(next: count => {
+    //   console.log(count);
+    // });
+
+    // CUSTOM OBSERVABLE
+    // const customIntervalObservable = Observable.create(observer => {
+    //   let count=0;
+    //   setInterval( handler: () => {
+    //     observer.next(count);
+    //     count++;
+    //     if(count === 2) {
+    //       observer.complete();
+    //     }
+
+    //     if(count > 3) {
+    //       observer.error(new Error(message: ' ERRPR ENCOUNTERED!'));
+    //     }
+    //   }, timeout: 1000);
+    // });
+
+    // this.firstObsSubscriptions = customIntervalObservable.pipe(map(project: (data :number) => {
+    //   return 'Round ' + (data + 1);
+    // })).subscribe(data => {
+    //   console.log(data);
+    // }, error => {
+    //   console.log(error);
+    // }, () => {
+    //   console.log("completed");
+    // });
   }
+
+  // ngOnDestroy(): void {
+  //   this.firstObsSubscriptions.unsubscribe();
+  // }
 
 }
