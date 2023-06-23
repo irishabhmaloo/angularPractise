2aca813854ea3ec60b82a37fd4e64c090a8b01b7
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-23 14:01:50 +0530

    final before DELETE http request

--- src/app/http-form/http-form.component.ts
+++ src/app/http-form/http-form.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-http-form',
@@ -9,48 +11,36 @@ import { Post } from './post.model';
   styleUrls: ['./http-form.component.css']
 })
 export class HttpFormComponent {
+  @ViewChild('postForm') myForm: NgForm;
+  
   loadedPosts: Post[] = [];
+  isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onCreatePost(postData: Post) {
-    // Send Http request
-    console.log(postData);
-    this.http.post('https://learnangular-86b7b-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', 
-    postData).subscribe(responseData => {
-      console.log(responseData);
-    });
+    this.postsService.createAndStorePost(postData.title , postData.content);
+    this.myForm.reset();
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.http
-    .get<{[key: string] : Post}>('https://learnangular-86b7b-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
-    .pipe(map(responseData => {
-      const postsArray: Post[] = [];
-      for(const key in responseData) {
-        if(responseData.hasOwnProperty(key)) 
-        //so that you are not trying to access the properties of some prototype
-        {
-          postsArray.push({...responseData[key], id:key});
-        }
-      } 
-      return postsArray; //for subscribe
-    }))
-    .subscribe(posts => {
-      this.loadedPosts = posts;
-    });
   }
 }
---
+++ src/app/http-form/posts.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { map } from "rxjs/operators";
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+    
+    constructor(private http: HttpClient) {}
+
+    createAndStorePost(title: string, content: string ) {
+        // Send Http request
+        const postData: Post = {title: title, content: content}
+
+        console.log(postData);
+        this.http.post('https://learnangular-86b7b-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', 
+        postData).subscribe(responseData => {
+        console.log(responseData);
+        });
+    }
+
+    fetchPosts() {
+        return this.http
+            .get<{[key: string] : Post}>('https://learnangular-86b7b-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+            .pipe(map(responseData => {
+            const postsArray: Post[] = [];
+            for(const key in responseData) {
+                if(responseData.hasOwnProperty(key)) 
+                //so that you are not trying to access the properties of some prototype
+                {
+                postsArray.push({...responseData[key], id:key});
+                }
+            } 
+            return postsArray; //for subscribe
+            }));
+    }
+}
